a16afcd26cc864b9491c01bf056a1ed845803d23
Garrett Smith <g.smith61900@gmail.com>
2021-03-14 18:02:11 -0700

    init commit

---
+++ GS_M02_DV01_02_NavMesh/Assets/Scripts/AgentControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentControl : MonoBehaviour
+{
+    public Transform home;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(home.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GS_M02_DV01_02_NavMesh/Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    bool open = false;
+    public GameObject link;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (open)
+            {
+                this.transform.Translate(0, -5f, 0);
+                link.SetActive(false);
+            }
+            else
+            {
+                this.transform.Translate(0, 5f, 0);
+                link.SetActive(true);
+            }
+
+            open = !open;
+        }
+    }
+}
